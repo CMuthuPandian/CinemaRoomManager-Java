29c4d373df8c26c96497d85ce0356ac3684c7fd6
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-14 00:14:09 +0530

    Update CinemaRoomManager.java

--- CinemaRoomManager.java
+++ CinemaRoomManager.java
@@ -147,3 +147,205 @@ public class CinemaRoomManager {
 
 }
 
+/*
+
+Output:
+
+Enter the number of rows:
+9
+Enter the number of seats in each row:
+9
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+1
+
+Cinema:
+  1 2 3 4 5 6 7 8 9 
+1 S S S S S S S S S 
+2 S S S S S S S S S 
+3 S S S S S S S S S 
+4 S S S S S S S S S 
+5 S S S S S S S S S 
+6 S S S S S S S S S 
+7 S S S S S S S S S 
+8 S S S S S S S S S 
+9 S S S S S S S S S 
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+3
+
+Number of purchased tickets: 0
+Percentage: 0.00%
+Current income: $0
+Total income:$720
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+2
+
+Enter a row number:
+3
+Enter a seat number in that row:
+3
+
+Ticket price: $10
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+1
+
+Cinema:
+  1 2 3 4 5 6 7 8 9 
+1 S S S S S S S S S 
+2 S S S S S S S S S 
+3 S S B S S S S S S 
+4 S S S S S S S S S 
+5 S S S S S S S S S 
+6 S S S S S S S S S 
+7 S S S S S S S S S 
+8 S S S S S S S S S 
+9 S S S S S S S S S 
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+3
+
+Number of purchased tickets: 1
+Percentage: 1.23%
+Current income: $10
+Total income:$720
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+2
+
+Enter a row number:
+5
+Enter a seat number in that row:
+5
+
+Ticket price: $8
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+2
+
+Enter a row number:
+5
+Enter a seat number in that row:
+5
+
+That ticket has already been purchased!
+
+Enter a row number:
+10
+Enter a seat number in that row:
+20
+
+Wrong input!
+
+Enter a row number:
+6
+Enter a seat number in that row:
+8
+
+Ticket price: $8
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+3
+
+Number of purchased tickets: 3
+Percentage: 3.70%
+Current income: $26
+Total income:$720
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+2
+
+Enter a row number:
+4
+Enter a seat number in that row:
+7
+
+Ticket price: $10
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+8
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+2
+
+Enter a row number:
+8
+Enter a seat number in that row:
+2
+
+Ticket price: $8
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+3
+
+Number of purchased tickets: 5
+Percentage: 6.17%
+Current income: $44
+Total income:$720
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+1
+
+Cinema:
+  1 2 3 4 5 6 7 8 9 
+1 S S S S S S S S S 
+2 S S S S S S S S S 
+3 S S B S S S S S S 
+4 S S S S S S B S S 
+5 S S S S B S S S S 
+6 S S S S S S S B S 
+7 S S S S S S S S S 
+8 S B S S S S S S S 
+9 S S S S S S S S S 
+
+1. Show the seats
+2. Buy a ticket
+3. Statistics
+0. Exit
+0
+
+
+Process finished with exit code 0
+
+ */
